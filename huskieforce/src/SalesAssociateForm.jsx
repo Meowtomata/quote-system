ae8b0c506257aa3b03450afc5b49b1c29cc79359
Bleon Azemi <azemibleon03@gmail.com>
2025-04-29 21:35:37 -0500

    Edit and Adding
    Added the ability to edit and add sales associate information

--- huskieforce/src/SalesAssociateForm.jsx
+++ huskieforce/src/SalesAssociateForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-function SalesAssociateForm({ onSave, initial }) {
+function SalesAssociateForm({ onSave, initial, editing, onCancel, clearFormTrigger }) {
   const [form, setForm] = useState({
     name: "",
     userId: "",
@@ -10,8 +10,28 @@ function SalesAssociateForm({ onSave, initial }) {
   });
 
   useEffect(() => {
-    if (initial) setForm(initial);
+    if (initial) {
+      setForm({
+        name: initial.Name,
+        userId: initial.User_ID,
+        password: initial.Password,
+        commission: initial.Accumulated_Commission,
+        address: initial.Address,
+      });
+    }
   }, [initial]);
+
+  useEffect(() => {
+    setForm({
+      name: "",
+      userId: "",
+      password: "",
+      commission: "",
+      address: ""
+    });
+  }, [clearFormTrigger]);
+  
+  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,20 +39,41 @@ function SalesAssociateForm({ onSave, initial }) {
   };
 
   const handleSubmit = () => {
-    onSave({ ...form, commission: parseFloat(form.commission) });
+    if (!form.name || !form.userId || !form.password || !form.address) {
+      alert("Please fill in all fields.");
+      return;
+    }
+  
+    const associatePayload = {
+      name: form.name,
+      userId: form.userId,
+      password: form.password,
+      address: form.address,
+      accumulatedCommission: parseFloat(form.commission)
+    };
+  
+    onSave(associatePayload); 
     setForm({ name: "", userId: "", password: "", commission: "", address: "" });
   };
+  
 
   return (
     <div className="admin-container">
-      <h3>ADD ASSOCIATE(S)</h3>
+      <h3>{editing ? "UPDATE ASSOCIATE INFORMATION" : "ADD ASSOCIATE(S)"}</h3>
       <div className="AddAssoc">
       <input name="name" placeholder="NAME:" value={form.name} onChange={handleChange} />
       <input name="userId" placeholder="USER ID:" value={form.userId} onChange={handleChange} />
       <input name="password" placeholder="PASSWORD:" value={form.password} onChange={handleChange} />
       <input name="commission" placeholder="COMMISSION:" value={form.commission} onChange={handleChange} />
       <input name="address" placeholder="ADDRESS:" value={form.address} onChange={handleChange} />
+      <div className="button-group">
       <button className="Add" onClick={handleSubmit}>{initial ? "UPDATE" : "ADD"}</button>
+      {editing && (
+  <button className="Cancel" onClick={onCancel}>
+    CANCEL
+  </button>
+      )}
+      </div>
     </div>
     </div>
   );
